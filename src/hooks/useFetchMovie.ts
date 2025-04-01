bc79c14acfae159cf0a509b1efc3582fd1993ddd
Zombiemas <maks.smerdov25@gmail.com>
2025-04-01 21:17:50 +0500

    add intro, bestfilms, hook

---
+++ src/hooks/useFetchMovie.ts
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export const useFetchMovie = <T>(
+  api: string
+): { loading: boolean; data: T | null; error: boolean } => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<boolean>(false);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(`https://cinemaguide.skillbox.cc/${api}`);
+      const result = await response.json();
+      setData(result);
+      setError(false);
+      setLoading(false);
+    } catch (error) {
+      console.error('Ошибка при загрузке данных:', error);
+      setLoading(false);
+      setError(true);
+    }
+  }, [api]);
+
+  useEffect(() => {
+    void fetchData();
+    const intervalId = setInterval(() => {
+      void fetchData();
+    });
+    return () => clearInterval(intervalId);
+  }, [fetchData]);
+
+  return { loading, data, error };
+};
